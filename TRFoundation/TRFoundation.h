f85b9baf5e907df5714f2b553b0a035e117b481c
Podmno <podmno@icloud.com>
2024-03-26 10:32:39 +0800

    Initial Commit

---
+++ TRFoundation/TRFoundation.h
@@ -0,0 +1,18 @@
+//
+//  TRFoundation.h
+//  TRFoundation
+//
+//  Created by Ki MNO on 2024/3/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TRFoundation.
+FOUNDATION_EXPORT double TRFoundationVersionNumber;
+
+//! Project version string for TRFoundation.
+FOUNDATION_EXPORT const unsigned char TRFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRFoundation/PublicHeader.h>
+
+
